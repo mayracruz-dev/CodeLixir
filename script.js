9a7a3d3ee63da6f8c26f4bc46f6ca42eb47de0a2
mayracruz-dev <78674928+mayracruz-dev@users.noreply.github.com>
2024-09-06 17:14:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("¡Gracias por contactarnos! Nos pondremos en contacto contigo pronto.");
+});
